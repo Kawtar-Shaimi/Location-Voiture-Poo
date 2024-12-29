1b5dec2565c636a625bba27dac8526db6cbca87a
Kawtar <Kawtar.shaimi8@gmail.com>
2024-12-29 19:24:43 +0100

    Finish Project

--- class/contract.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'BaseCrud.php';
-
-class Contrat extends BaseCrud {
-    public function __construct() {
-        parent::__construct('contrats');
-    }
-}
-echo "haaaaaaaay ther";
